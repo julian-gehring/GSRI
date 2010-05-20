1447cfa32de96bcb97aad8e747e0a9b7cc3c17fd
j.gehring <j.gehring@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-05-20 12:34:44 +0000

    updated code and plotting colors
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GSRI@47047 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/gsriCode.R
+++ R/gsriCode.R
@@ -6,25 +6,26 @@ function (pvals, useGrenander)
     ord <- sort.list(pvals, method = "quick", na.last = NA)
     pvals <- pvals[ord]
     uniquePvals <- unique(pvals)
-    rcdf <- ecdf(pvals)
+    rcdf <- stats::ecdf(pvals)
     x <- environment(rcdf)$x
     cdf <- environment(rcdf)$y
-    if (useGrenander == TRUE)
-      cdf <- grenanderInterp(x, cdf)
+    if (useGrenander == TRUE) 
+        cdf <- GSRI:::grenanderInterp(x, cdf)
     if (nGenes != length(uniquePvals)) {
-      tp <- as.numeric(table(pvals))
-      w <- diff(c(0, cdf))/tp
-      cdf <- rep.int(cdf, tp)
+        tp <- as.numeric(table(pvals))
+        w <- diff(c(0, cdf))/tp
+        cdf <- rep.int(cdf, tp)
     }
     cdf <- cdf - cor
-    res <- list(sortedPvals=pvals, cdf=cdf)
+    res <- list(sortedPvals = pvals, cdf = cdf)
     return(res)
 }
 getPvalues <-
 function (data, d, phenotype, test, testArgs) 
 {
     phenotype <- phenotype[d]
-    pvals <- multiStat(as.matrix(data), phenotype, test, testArgs)
+    pvals <- GSRI:::multiStat(as.matrix(data), phenotype, test, 
+        testArgs)
     pvals <- pvals[!is.na(pvals)]
     return(pvals)
 }
@@ -50,9 +51,9 @@ function (data, phenotype, geneSetName, useGrenander = FALSE,
     plotResults = TRUE, writeResults = FALSE, nBootstraps = 100, 
     test = "ttest", testArgs = NULL, alpha = 0.05, prec = 4) 
 {
-    nSamples <- dim(data)[2]
+    nSamples <- ncol(data)
     if (!is.function(test) && !is.character(test)) 
-        stop("Argument 'test' must be of class function or character")
+        stop("Argument 'test' must be of class 'function' or 'character'")
     if (is.character(test) && is.na(pmatch(test, c("ttest", "ftest"), 
         NA))) 
         stop("Character string for 'test' must be 'ttest' or 'ftest'")
@@ -60,38 +61,38 @@ function (data, phenotype, geneSetName, useGrenander = FALSE,
         stop("Sizes of 'data' and 'phenotype' do not match.")
     if (!is.factor(phenotype)) 
         phenotype <- factor(phenotype)
-    b <- boot(t(data), gsriBoot, nBootstraps, phenotype = phenotype, 
+    b <- boot::boot(t(data), GSRI:::gsriBoot, nBootstraps, phenotype = phenotype, 
         useGrenander = useGrenander, test = test, testArgs = testArgs)
     p <- max(b$t0[[1]], 0)
     psd <- sd(b$t[, 1])
     bias <- apply(b$t, 2, mean) - b$t0
     pt <- b$t - bias
-    gsri <- quantile(pt, alpha, na.rm=TRUE)
-    d <- 1:nSamples
-    pvals <- getPvalues(data, d, phenotype, test, testArgs)
+    gsri <- max(quantile(pt, alpha, na.rm = TRUE), 0)
+    pvals <- GSRI:::getPvalues(data, 1:nSamples, phenotype, test, 
+        testArgs)
     nGenes <- length(pvals)
-    res <- getCDF(pvals, useGrenander)
+    res <- GSRI:::getCDF(pvals, useGrenander)
     numRegGenes <- p * nGenes
     numRegGenesSd <- psd * nGenes
-    if (plotResults == TRUE)
-      plotResults(res, geneSetName, p, gsri)
-    if (writeResults == TRUE)
-      writeResults(res, geneSetName, p, prec)
-    result <- list(geneSet=geneSetName, percRegGenes=p, percRegGenesSd=psd,
-        numRegGenes=numRegGenes, numRegGenesSd=numRegGenesSd, gsri=gsri,
-        nGenes=nGenes)
+    if (plotResults == TRUE) 
+        GSRI:::plotResults(res, geneSetName, p, gsri)
+    if (writeResults == TRUE) 
+        GSRI:::writeResults(res, geneSetName, p, prec)
+    result <- list(geneSet = geneSetName, percRegGenes = p, percRegGenesSd = psd, 
+        numRegGenes = numRegGenes, numRegGenesSd = numRegGenesSd, 
+        gsri = gsri, nGenes = nGenes)
     return(result)
 }
 gsriBoot <-
 function (inputData, d, phenotype, useGrenander, test, testArgs) 
 {
     data <- t(inputData)
-    pvals <- getPvalues(data, d, phenotype, test, testArgs)
+    pvals <- GSRI:::getPvalues(data, d, phenotype, test, testArgs)
     nValidGenes <- length(pvals)
     maxIter <- nValidGenes + 2
     p <- 1
     restOld <- 0
-    res <- getCDF(pvals, useGrenander)
+    res <- GSRI:::getCDF(pvals, useGrenander)
     for (nIterate in 1:maxIter) {
         rest <- nValidGenes - ceiling(p * nValidGenes)
         rest <- max(c(restOld, rest, 1))
@@ -100,10 +101,10 @@ function (inputData, d, phenotype, useGrenander, test, testArgs)
             break
         x <- res$sortedPvals[rest:nValidGenes] - 1
         y <- res$cdf[rest:nValidGenes] - 1
-        p <- slopeFast(x, y)
+        p <- GSRI:::slopeFast(x, y)
         restOld <- rest
-        if(is.na(p))
-          break
+        if (is.na(p)) 
+            break
     }
     erg <- 1 - p
     return(erg)
@@ -115,60 +116,57 @@ function (dataFileName, phenotypeFileName, geneSetFileName, useGrenander = FALSE
     alpha = 0.05, verbose = TRUE, prec = 4) 
 {
     if (file.exists(dataFileName) == FALSE) 
-        stop(sprintf("%s '%s' %s", "File", dataFileName, "cannot be accessed."))
+        stop(sprintf("%s '%s' %s", "File", dataFileName, "cannot be read"))
     if (file.exists(phenotypeFileName) == FALSE) 
         stop(sprintf("%s '%s' %s", "File", phenotypeFileName, 
-            "cannot be read."))
+            "cannot be read"))
     if (file.exists(geneSetFileName) == FALSE) 
-        stop(sprintf("%s '%s' %s", "File", geneSetFileName, "cannot be read."))
-    data <- loadGct(dataFileName)
+        stop(sprintf("%s '%s' %s", "File", geneSetFileName, "cannot be read"))
+    data <- GSRI:::loadGct(dataFileName)
     if (verbose == TRUE) 
         message(sprintf("%s '%s' %s", "Data file", basename(dataFileName), 
             "loaded"))
-    phenotype <- loadCls(phenotypeFileName)
+    phenotype <- GSRI:::loadCls(phenotypeFileName)
     if (verbose == TRUE) 
-        message(sprintf("%s '%s' %s", "Phenotype file", basename(phenotypeFileName), 
-            "loaded"))
+        message(sprintf("%s '%s' %s", "Phenotype file",
+                        basename(phenotypeFileName), "loaded"))
     geneSets <- readLines(geneSetFileName)
     res <- list()
     c <- 1
     for (i in 1:length(geneSets)) {
         temp <- strsplit(geneSets[i], "\t")[[1]]
         geneSetName <- temp[1]
-        geneSet <- list()
-        geneSet$genes <- temp[3:length(temp)]
-        geneSet$data <- data[intersect(row.names(data), geneSet$genes), 
+        gsData <- data[intersect(row.names(data), temp[3:length(temp)]), 
             ]
-        geneSet$genes <- row.names(geneSet$data)
-        geneSet$genesResults <- paste(row.names(geneSet$data), 
-            collapse = " : ")
-        geneSetSize <- dim(geneSet$data)[1]
-        if (geneSetSize >= minGeneSetSize) {
+        gsSetSize <- nrow(gsData)
+        gsGenesResults <- paste(row.names(gsData), collapse = " : ")
+        if (gsSetSize >= minGeneSetSize) {
             if (verbose == TRUE) 
-                message(sprintf("%s '%s' (%d %s)", "Calculate GSRI for", 
-                  geneSetName, geneSetSize, "genes"))
-            gsriRes <- gsri(geneSet$data, phenotype, geneSetName, 
+                message(sprintf("%s '%s' (%d %s)", "Calculating GSRI for", 
+                  geneSetName, gsSetSize, "genes"))
+            gsriRes <- GSRI::gsri(gsData, phenotype, geneSetName, 
                 useGrenander = useGrenander, plotResults = plotResults, 
                 writeResults = writeResults, test = test, testArgs = testArgs, 
                 alpha = alpha, nBootstraps = nBootstraps, prec = prec)
             res$geneSet[c] <- gsriRes$geneSet
             res$percRegGenes[c] <- signif(gsriRes$percRegGenes, 
-                digits = prec)
+                prec)
             res$percRegGenesSd[c] <- signif(gsriRes$percRegGenesSd, 
-                digits = prec)
+                prec)
             res$numRegGenes[c] <- signif(gsriRes$numRegGenes, 
-                digits = prec)
+                prec)
             res$numRegGenesSd[c] <- signif(gsriRes$numRegGenesSd, 
-                digits = prec)
-            res$gsri[c] <- signif(gsriRes$gsri, digits = prec)
-            res$nGenes[c] <- signif(gsriRes$nGenes, digits = prec)
-            res$geneSetGenes[c] <- geneSet$genesResults
+                prec)
+            res$gsri[c] <- signif(gsriRes$gsri, prec)
+            res$nGenes[c] <- signif(gsriRes$nGenes, prec)
+            res$geneSetGenes[c] <- gsGenesResults
             c <- c + 1
         }
     }
     if (writeSummary == TRUE) {
-        resultFile <- file.path(getwd(), paste(strsplit(dataFileName, 
-            ".gct")[[1]], "_results.txt", sep = ""))
+        resultFileName <- paste(strsplit(dataFileName, ".gct")[[1]], 
+            "_results.txt", sep = "")
+        resultFile <- file.path(getwd(), resultFileName)
         write.table(res, file = resultFile, quote = TRUE, row.names = FALSE, 
             sep = "\t")
     }
@@ -201,15 +199,37 @@ function (fileName)
 multiStat <-
 function (data, phenotype, test, testArgs) 
 {
-    if (!is.function(test) && test == "ttest") 
-        pvals <- rowttests(data, phenotype)$p.value
-    if (!is.function(test) && test == "ftest") 
-        pvals <- rowFtests(data, phenotype)$p.value
-    if (is.function(test) && is.null(testArgs)) 
-        pvals <- test(data, phenotype)
-    if (is.function(test) && !is.null(testArgs)) 
-        pvals <- test(data, phenotype, testArgs)
+    if (!is.function(test)) {
+        if (test == "ttest") 
+            pvals <- genefilter::rowttests(data, phenotype)$p.value
+        if (test == "ftest") 
+            pvals <- genefilter::rowFtests(data, phenotype)$p.value
+    }
+    else {
+        if (is.null(testArgs)) 
+            pvals <- test(data, phenotype)
+        else pvals <- test(data, phenotype, testArgs)
+    }
     return(pvals)
+}
+plotResults <-
+function (res, geneSetName, p, gsri) 
+{
+    xfit <- c(0, 1)
+    yfit <- p + (1 - p) * xfit
+    graphics::plot(c(0, 1), c(p, p), col = "red", type = "l", 
+        lty = 2, xlab = "p-values", ylab = "CDF(p)", main = geneSetName, 
+        xlim = c(0, 1), ylim = c(0, 1))
+    graphics::lines(c(0, 1), c(gsri[1], gsri[1]), col = "blue", 
+        lty = 2)
+    graphics::lines(xfit, yfit, col = "gray")
+    graphics::points(res$sortedPvals, res$cdf, xlab = "p-values", 
+        ylab = "CDF(p)", main = geneSetName, xlim = c(0, 1), 
+        ylim = c(0, 1), pch = 20)
+    graphics::text(1, p + 0.01, sprintf("%s=%.2f", "%RegGene", 
+        p), cex = 0.8, adj = c(1, 0))
+    graphics::text(1, gsri[1] - 0.01, sprintf("%s=%.2f", "GSRI", 
+        gsri[1]), cex = 0.8, adj = c(1, 1))
 }
 slopeFast <-
 function (x, y) 
@@ -218,28 +238,16 @@ function (x, y)
     b <- as.numeric((tx %*% y)/(tx %*% x))
     return(b)
 }
-
-plotResults <- function(res, geneSetName, p, gsri)  {
-  plot(res$sortedPvals, res$cdf, xlab = "p-values", ylab = "CDF(p)", 
-       main = geneSetName, xlim = c(0, 1), ylim = c(0, 1), pch = 20)
-  lines(c(0, 1), c(p, p), col = "red", lty = 2)
-  lines(c(0, 1), c(gsri[1], gsri[1]), col = "green", lty = 2)
-  text(1, p, sprintf("%s=%.2f", "%RegGene", p), cex = 0.8, 
-       adj = c(1, 0))
-  text(1, gsri[1], sprintf("%s=%.2f", "GSRI", gsri[1]), 
-       cex = 0.8, adj = c(1, 1))
-  xfit <- c(0, 1)
-  yfit <- p + (1 - p) * xfit
-  lines(xfit, yfit)
-}
-
-writeResults <- function(res, geneSetName, p, prec)  {
-  xfit <- res$sortedPvals
-  yfit <- p + (1 - p) * xfit
-  dataFileName <- paste("GeneSet_", geneSetName, "_data.txt", sep = "")
-  dataResults <- list(sorted_pvals=signif(res$sortedPvals, digits = prec),
-                      cdf=signif(res$cdf, digits = prec),
-                      fit=signif(yfit, digits = prec))
-  write.table(dataResults, file = dataFileName, quote = FALSE, 
-              row.names = FALSE, sep = "\t")
+writeResults <-
+function (res, geneSetName, p, prec) 
+{
+    xfit <- res$sortedPvals
+    yfit <- p + (1 - p) * xfit
+    dataFileName <- paste("GeneSet_", geneSetName, "_data.txt", 
+        sep = "")
+    dataResults <- list(sorted_pvals = signif(res$sortedPvals, 
+        prec), cdf = signif(res$cdf, prec), fit = signif(yfit, 
+        prec))
+    write.table(dataResults, file = dataFileName, quote = FALSE, 
+        row.names = FALSE, sep = "\t")
 }
