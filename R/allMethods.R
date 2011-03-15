f1896da6d35ef2db5b9529e1161ce6edb8f2c7b3
j.gehring <j.gehring@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-03-15 09:08:48 +0000

    add methods for 'Gsri' class
    add 'show', 'getGsri', 'getParms', 'plot', 'export' for class 'Gsri'
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GSRI@53720 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/allMethods.R
+++ R/allMethods.R
@@ -1,3 +1,68 @@
+setMethod("show",
+          signature("Gsri"),
+          function(object) {
+            print(object@result)
+          })
+
+setGeneric("getGsri",
+           function(object)
+           standardGeneric("getGsri"))
+
+setMethod("getGsri",
+          signature("Gsri"),
+          function(object) {
+            object@result
+          })
+
+
+setGeneric("getParms",
+           function(object)
+           standardGeneric("getParms"))
+
+setMethod("getParms",
+          signature("Gsri"),
+          function(object) {
+            data.frame(object@parms, row.names="")
+          })
+
+setMethod("plot",
+          signature("Gsri", "ANY"),
+          function(x, y, digits=2, ...) {
+            ##ind <- which(getGsr(x)  getGsri(x)[y, ])
+            ind <- y
+            if(length(ind) != 1)
+              stop("'y' must have exactly one match in 'x'.")
+            result <- getGsri(x)[ind, ]
+            p <- as.numeric(result[1])
+            g <- as.numeric(result[3])
+            
+            graphics::plot(c(0, 1), c(p, p), col="red", type="l", 
+                           lty=2, xlab="p-values", ylab="CDF(p)", main=rownames(result), 
+                           xlim=c(0, 1), ylim=c(0, 1), ...)
+            graphics::lines(c(0, 1), c(g, g), col="blue", lty=2)
+            graphics::lines(c(0,1), p+(1-p)*c(0,1), col="gray")
+            graphics::points(x@pval[[ind]], x@cdf[[ind]], pch=20)
+            format <- paste("%s=%.", digits, "f", sep="")
+            graphics::text(1, p+0.01, sprintf(format, "%RegGene", p),
+                           cex=0.8, adj=c(1, 0))
+            graphics::text(1, g-0.01, sprintf(format, "GSRI", g),
+                           cex=0.8, adj=c(1,1))
+            
+          })
+
+
+setGeneric("export",
+           function(object, file, ...)
+           standardGeneric("export"))
+
+setMethod("export",
+          signature("Gsri", "character"),
+          function(object, file, digits=Inf) {
+            result <- round(getGsri(object), digits)
+            write.table(result, file, sep="\t")
+          })
+
+
 setGeneric("readCls",
            function(file, ...)
            standardGeneric("readCls"))
