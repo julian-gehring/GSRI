ae93719b1cff19c184060e60c001646bff15d2e4
j.gehring <j.gehring@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-03-15 09:10:23 +0000

    add limma test functions
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GSRI@53734 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/allExportedFunctions.R
+++ R/allExportedFunctions.R
@@ -4,3 +4,27 @@ rowt <- function(exprs, groups, id, testArgs)
 
 rowF <- function(exprs, groups, id, testArgs)
   genefilter::rowFtests(exprs[id, ], groups)$p.value
+
+
+limmat <- function(exprs, groups, id, testArgs) {
+  design <- cbind(offset=1, diff=groups)
+  fit <- limma::lmFit(exprs, design)
+  fit <- limma::eBayes(fit)
+  pval <- fit$p.value[id,"diff"]
+
+  return(pval)
+}
+
+
+limmatGsri <- function(exprs, groups, id, testArgs) {
+  design <- cbind(offset=1, diff=groups)
+  fit <- limma::lmFit(exprs, design)
+  fit <- limma::eBayes(fit)
+  pval <- fit$p.value[ ,"diff"]
+
+  p0 <- les:::gsri(pval, grenander=testArgs$grenander, se=FALSE)[1]
+  fit <- limma::eBayes(fit, p0)
+  pval <- fit$p.value[id,"diff"]
+
+  return(pval)
+}
