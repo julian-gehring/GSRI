a46bd00229c24076505db5da59f34178d33975ba
j.gehring <j.gehring@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-03-15 09:10:33 +0000

    switch to new bootstrap method, minor fixes
    switch to bootstrapping within groups
    add check for same length of 'id' and 'pval'
    tidy up
    update man pages
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GSRI@53736 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/allInternal.R
+++ R/allInternal.R
@@ -1,8 +1,8 @@
-calcGsri <- function(exprs, groups, name, id, 
-                     weight, grenander=TRUE, nBoot=100,
+calcGsri <- function(exprs, groups, name, id, weights,
+                     grenander=TRUE, nBoot=100,
                      test="ttest", testArgs=NULL, alpha=0.05, ...) {
 
-  if(length(id) == 0 || !all(is.logical(id)))
+  if(length(id) == 0 || !is.logical(id))
     stop("Gene set has no matches in the expression data.")
   if(ncol(exprs) != length(groups))
     stop("Number of columns of 'exprs' must match 'groups'.")
@@ -12,34 +12,24 @@ calcGsri <- function(exprs, groups, name, id,
   
   pval <- multiStat(exprs, groups, id, grenander, test, testArgs)
   nPval <- length(pval)
-  if(is.null(weight))
-    weight <- rep(1, nPval)
-  les <- les:::fitGsri(pval, NULL, weight, nPval, grenander, se=TRUE, custom=FALSE)
-  p0 <- les[1]
-  psd0 <- les[2]
+  if(is.null(weights))
+    weights <- rep(1/nGenesGs, nGenesGs)
+  if((length(weights) != nGenesGs) || (length(weights) != nPval))
+    stop("'weight' must contain one value for each gene in the gene set.")
+  les <- les:::fitGsri(pval, NULL, weights, nPval, grenander, se=FALSE, custom=FALSE)
+  p <- les[1]
   
-  ## pval <- multiStat(exprs, 1:ncol(exprs), groups, test, testArgs)
-  b <- boot::boot(t(exprs), gsriBoot, nBoot,
-                  groups=groups, id=id, cweight=weight, 
-                  grenander=grenander, test=test, testArgs=testArgs)
-  p1 <- max(b$t0[[1]], 0)
-  bias <- apply(b$t, 2, mean) - b$t0
-  pt <- b$t - bias
-  gsri <- max(stats::quantile(pt, alpha, na.rm=TRUE), 0)
-  
-  p <- p1
-  #pb <- replicate(nBoot,
-  #                gsriBoot(exprs, groups, weight, grenander, test, testArgs))
-  #p <- max(b$t0, 0)
-  #psd <- stats::sd(pb)
-  #gsri <- max(stats::quantile(pb, alpha, na.rm=TRUE), 0)
+  pb <- replicate(nBoot, gsriBoot(exprs, groups, weights, id, grenander, test, testArgs))
+  pb <- pb - mean(pb) + p
+  psd <- stats::sd(pb)
+  gsri <- max(stats::quantile(pb, alpha, na.rm=TRUE), 0)
+
   nRegGenes <- as.integer(floor(p*nGenesGs))
-  result <- data.frame(pRegGenes=p0, pRegGenesSd=psd0, nRegGenes=nRegGenes,
+  result <- data.frame(pRegGenes=p, pRegGenesSd=psd, nRegGenes=nRegGenes,
                        gsri=gsri, nGenes=nGenesGs, row.names=name)
   names(result)[4] <- sprintf("%s(%g%%)", "GSRI", alpha*100)
   res <- list(result=result, pval=pval)
-  #res <- result
-  
+
   return(res)
 }
 
@@ -47,35 +37,29 @@ calcGsri <- function(exprs, groups, name, id,
 multiStat <- function(exprs, groups, id, grenander, test, testArgs) {
 
   pval <- test(exprs, groups, id, testArgs)
-  pval <- pval[!is.na(pval)] ## needed?
+  if(length(pval) != sum(id))
+    stop("Test statistics must return one p-value for each gene in the gene set.")
+  #pval <- pval[!is.na(pval)] ## needed?
 
   return(pval)
 }
 
 
-gsriBoot <- function(exprs, index, groups, id, cweight, grenander, test, testArgs, ...) {
+gsriBoot <- function(exprs, groups, weights, id, grenander, test, testArgs) {
 
-  pval <- multiStat(t(exprs), groups[index], id, grenander, test, testArgs)
-  p <- les:::fitGsri(pval, NULL, cweight, length(pval), grenander, FALSE, FALSE)[1]
+  bo <- bootWithinGroup(exprs, groups)
+  pval <- multiStat(bo$exprs, bo$groups, id, grenander, test, testArgs)
+  res <- les:::fitGsri(pval, NULL, weights, length(pval), grenander, se=TRUE, custom=FALSE)[1]
 
-  return(p)
+  return(res)
 }
 
 
-#gsriBoot <- function(exprs, groups, weight, grenander, test, testArgs) {
-#
-#  boot <- bootWithinGroup(exprs, groups)
-#  res <- multiStat(boot$exprs, boot$label, weight, grenander, test, testArgs) ## weight[ord] !!!
-#
-#  return(res)
-#}
-
-
-bootWithinGroup <- function(exprs, groups) {
+bootWithinGroup <- function(exprs, groups, weights) {
 
   ord <- order(groups)
-  exprs <- exprs[ ,ord, drop=FALSE]
-  groups <- groups[ord]
+  exprs <- exprs[ ,ord,drop=FALSE]
+  groups <- groups[ord] ## needed for correct resampling
 
   nLevels <- nlevels(groups) ## or length(nSamples) ? factor w/o group?
   nSamples <- tabulate(groups)
@@ -84,12 +68,11 @@ bootWithinGroup <- function(exprs, groups) {
   offset <- rep.int(cumsum(c(0, nSamples[-length(nSamples)])), nSamples)
   ordBoot <- perm + offset
 
-  exprs <- exprs[ ,ordBoot, drop=FALSE]
-  groupsOrd <- groups[ordBoot]
-  if(!identical(groups, groupsOrd))
+  exprs <- exprs[ ,ordBoot,drop=FALSE]
+  if(!identical(groups, groups[ordBoot])) ## remove? just control
     warning("Bootstrapping lead to changes in the groups.")
+  res <- list(exprs=exprs, groups=groups)
 
-  return(list(exprs=exprs, groups=groupsOrd))
+  return(res)
 }
-
 
